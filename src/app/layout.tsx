6d66921248b2dd217645071147294a62060c4f2e
leloufadel <leloufadel@gmail.com>
2025-10-27 13:09:32 +0000

    update facon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,17 @@ import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import AOSInit from "@/components/AOSInit";
+export const metadata = {
+  title: "MTC",
+  icons: {
+    icon: [
+      { url: "/mtc-logo-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/mtc-logo-32x32.png", sizes: "32x32", type: "image/png" },
+    ],
+    apple: "/mtc-logo-180x180.png",
+  },
+};
+
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
